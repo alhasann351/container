bf176433bebf6fa68f67ab2931ac871a088a28c5
alhasann351 <alhasann352@gmail.com>
2024-02-13 21:19:07 +0600

    container added

--- lib/main.dart
+++ lib/main.dart
@@ -41,8 +41,100 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ),
       ),
-      body: const Center(
-        child: Text('Container'),
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(15),
+                  //shape: BoxShape.rectangle,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.green.shade400,
+                      blurRadius: 20,
+                      offset: const Offset(5, 10),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                    border: const Border(
+                      bottom: BorderSide(width: 5, color: Colors.red),
+                      top: BorderSide(width: 5, color: Colors.red),
+                      left: BorderSide(width: 5, color: Colors.red),
+                      right: BorderSide(width: 5, color: Colors.red),
+                    )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 200,
+                height: 100,
+                decoration: const BoxDecoration(
+                  color: Colors.blueAccent,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                alignment: Alignment.center,
+                width: 200,
+                height: 100,
+                decoration: const BoxDecoration(
+                  color: Colors.yellow,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    bottomRight: Radius.circular(15),
+                  ),
+                  border: Border(
+                    bottom: BorderSide(width: 10, color: Colors.green),
+                    top: BorderSide(width: 10, color: Colors.green),
+                    left: BorderSide(width: 10, color: Colors.green),
+                    right: BorderSide(width: 10, color: Colors.green),
+                  ),
+                ),
+                child: const Text(
+                  'Border Container',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
